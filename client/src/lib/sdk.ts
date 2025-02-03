bac19ef595deb0a689a94bf537fabb0038fb0a50
olaf9723dev <olaf9723dev@gmail.com>
2025-02-03 13:34:15 -0600

    update

--- client/src/lib/sdk.ts
+++ client/src/lib/sdk.ts
@@ -1,6 +1,6 @@
 import { getStrapiURL } from "@/lib/utils";
-import { strapiSDK } from '@strapi/sdk-js';
+import { strapi } from '@strapi/sdk-js';
 
 const BASE_API_URL = getStrapiURL() + "/api";
-const sdk = strapiSDK({ baseURL: BASE_API_URL });
+const sdk = strapi({ baseURL: BASE_API_URL });
 export default sdk;
